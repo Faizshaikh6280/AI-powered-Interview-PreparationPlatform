e1a201ec1c047ea9e99dafe442aa4880e795008e
Faiz Alam <faizalam2432005@gmail.com>
2025-07-17 16:45:04 +0530

    Authentication and storage is done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ReactNode } from "react";
-
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign-in");
 
   return (
     <div className="root-layout">
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,18 +1,24 @@
 "use client";
-import React from "react";
 
 import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-
-import { Button } from "@/components/ui/button";
-
 import Link from "next/link";
 import Image from "next/image";
-import FormField from "./FormField";
-import { Form } from "./ui/form";
 import { toast } from "sonner";
+import { auth } from "@/firebase/client";
+import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+
+import { signIn, signUp } from "@/lib/actions/auth.action";
+import FormField from "./FormField";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -23,9 +29,9 @@ const authFormSchema = (type: FormType) => {
 };
 
 const AuthForm = ({ type }: { type: FormType }) => {
-  const formSchema = authFormSchema(type);
   const router = useRouter();
 
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -35,24 +41,60 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     try {
       if (type === "sign-up") {
-        toast.success("Account created successfully! Please sign in.");
-        router.push("/sign-in");
+        const { name, email, password } = data;
 
-        console.log("Sign-up values:", values);
-      } else if (type === "sign-in") {
-        toast.success("Signed in successfully!");
-        router.push("/dashboard");
-        console.log("Sign-in values:", values);
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        const { email, password } = data;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in Failed. Please try again.");
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
+        toast.success("Signed in successfully.");
+        router.push("/");
       }
     } catch (error) {
-      toast.error(`There was an error: ${error}`);
       console.log(error);
+      toast.error(`There was an error: ${error}`);
     }
-  }
+  };
+
   const isSignIn = type === "sign-in";
 
   return (
